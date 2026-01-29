1658881c8b02a8c5363acca54469366968f4df6d
sgendaneza-web <s.gendaneza@alustudent.comgit config --global user.email s.gendaneza@alustudent.com>
2026-01-29 21:57:34 +0200

    Added simulators and verified log generations

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -4,17 +4,22 @@ echo "Select log file to analyze."
 echo "1)Heart Rate"
 echo "2)Temperature"
 echo "3)Water Usage"
-read choice
+read -p "choice": choice
 
 case $choice in
 	1)log_file="hospital_data/active_logs/heart_rate.log";;
-	2)log_file="hospital_data/active_logs/temparature.log";;
+	2)log_file="hospital_data/active_logs/temperature.log";;
 	3)log_file="hopital_data/active_logs/water_usage.log";;
 	*)echo "invalid choice." 
 		exit 1;;
 esac
 
-echo "Analysis on $(2026-01-29)">> reports/analysis_report.txt
+if [ ! -f "$log_file" ]; then
+	echo "log_file does not exist: $log_file"
+	exit 1
+fi
+
+echo "Analysis on 2026-01-29">> reports/analysis_report.txt
 awk'{print $1}'"$log_file"|sort|uniq -c>> reports/analysis_report.txt
 echo "First entry:">> reports/analysis_report.txt
 head -n 1 "$log_file">> reports/analysis_report.txt
