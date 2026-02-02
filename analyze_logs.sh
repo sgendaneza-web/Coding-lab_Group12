dc94f1622c025b10cb0de74997252165bd4422bf
sgendaneza-web <s.gendaneza@alustudent.comgit config --global user.email s.gendaneza@alustudent.com>
2026-02-02 17:42:37 +0200

    archives

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -9,7 +9,7 @@ read -p "choice": choice
 case $choice in
 	1)log_file="hospital_data/active_logs/heart_rate.log";;
 	2)log_file="hospital_data/active_logs/temperature.log";;
-	3)log_file="hopital_data/active_logs/water_usage.log";;
+	3)log_file="hospital_data/active_logs/water_usage.log";;
 	*)echo "invalid choice." 
 		exit 1;;
 esac
@@ -20,10 +20,10 @@ if [ ! -f "$log_file" ]; then
 fi
 
 echo "Analysis on 2026-01-29">> reports/analysis_report.txt
-awk'{print $1}'"$log_file"|sort|uniq -c>> reports/analysis_report.txt
-echo "First entry:">> reports/analysis_report.txt
-head -n 1 "$log_file">> reports/analysis_report.txt
-echo "Last entry:">> reports/analysis_report.txt
-tail -n 1 "$log_file">> reports/analysis_report.txt
+awk '{print $1}' "$log_file" | sort |uniq -c >> reports/analysis_report.txt
+echo "First entry:" >> reports/analysis_report.txt
+head -n 1 "$log_file" >> reports/analysis_report.txt
+echo "Last entry:" >> reports/analysis_report.txt
+tail -n 1 "$log_file" >> reports/analysis_report.txt
 
 		
